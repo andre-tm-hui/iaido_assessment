0b61c44034f0008eb364d483f9f1c4b720d987d3
Andre Hui <andre.tm.hui@gmail.com>
2024-03-11 17:10:16 +0000

    removed generated files

--- lib/data/transactions/sources/transactions_dao.g.dart
+++
@@ -1,8 +0,0 @@
-// GENERATED CODE - DO NOT MODIFY BY HAND
-
-part of 'transactions_dao.dart';
-
-// ignore_for_file: type=lint
-mixin _$TransactionsDaoMixin on DatabaseAccessor<TransactionsDatabase> {
-  $TransactionsTable get transactions => attachedDatabase.transactions;
-}
